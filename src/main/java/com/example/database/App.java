085c0ba61abac6157d1ddb2eec7b1fae44e1a65d
Mark Gulacsy <mark.gulacsy@liferay.com>
2022-05-12 15:40:33 +0200

    Connect to mongo and list databases

--- src/main/java/com/example/database/App.java
+++ src/main/java/com/example/database/App.java
@@ -1,8 +1,18 @@
 package com.example.database;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println(PropertiesCache.getInstance().getProperty("mongodb.uri"));
-
+        String connectionString = PropertiesCache.getInstance().getProperty("mongodb.uri");
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+            databases.forEach(db -> System.out.println(db.toJson()));
+        }
     }
 }
