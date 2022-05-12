0e804a0a4b94e1f38e782d6e237a3536a8da6af6
Mark Gulacsy <mark.gulacsy@liferay.com>
2022-05-12 17:40:21 +0200

    Create MongoHandler

--- src/main/java/com/example/database/App.java
+++ src/main/java/com/example/database/App.java
@@ -1,21 +1,16 @@
 package com.example.database;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.bson.Document;
+import com.example.database.handler.MongoHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        String connectionString = PropertiesCache.getInstance().getProperty("mongodb.uri");
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
-            databases.forEach(db -> System.out.println(db.toJson()));
-        }
+        MongoHandler mongoHandler = new MongoHandler();
+        mongoHandler.connectToDatabase("test");
+        mongoHandler.printDatabaseStats();
+
+
     }
     private static final Logger _logger = LoggerFactory.getLogger(App.class.getName());
 }
