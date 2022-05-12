25ed9ddbf4cc1d1549e4a61f019c8ca93b17ef3b
Mark Gulacsy <mark.gulacsy@liferay.com>
2022-05-12 15:13:15 +0200

    Add propertis

--- src/main/java/com/example/database/App.java
+++ src/main/java/com/example/database/App.java
@@ -2,6 +2,7 @@ package com.example.database;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(PropertiesCache.getInstance().getProperty("mongodb.uri"));
+
     }
 }
--- src/main/java/com/example/database/PropertiesCache.java
+++ src/main/java/com/example/database/PropertiesCache.java
@@ -1,2 +1,45 @@
-package com.example.database;public class PropertiesCache {
+package com.example.database;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
+
+public class PropertiesCache
+{
+    private final Properties configProp = new Properties();
+
+    private PropertiesCache()
+    {
+        try (InputStream input = this.getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+            }
+            configProp.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class LazyHolder
+    {
+        private static final PropertiesCache INSTANCE = new PropertiesCache();
+    }
+
+    public static PropertiesCache getInstance()
+    {
+        return LazyHolder.INSTANCE;
+    }
+
+    public String getProperty(String key){
+        return configProp.getProperty(key);
+    }
+
+    public Set<String> getAllPropertyNames(){
+        return configProp.stringPropertyNames();
+    }
+
+    public boolean containsKey(String key){
+        return configProp.containsKey(key);
+    }
 }
